86a997b8c9b39ee1f96cc0086c4149fe0feec644
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-10-07 09:48:53 +0200

    Update PcTest.java

--- miOrdenador/src/com/ceep/mipc/PcTest.java
+++ miOrdenador/src/com/ceep/mipc/PcTest.java
@@ -6,12 +6,18 @@ public class PcTest {
 
     public static void main(String[] args) {
 
+        //Objetos PC
         Teclado t1 = new Teclado("USB", "Razer");
+        Raton r1 = new Raton("USB", "Razer");
+        Monitor m1 = new Monitor("Dell", 27.0);
+        Computadora pc1 = new Computadora("Gaming", m1, t1, r1);
 
-        Computadora pc1 = new Computadora();
-        Computadora pc2 = new Computadora();
-        Computadora pc3 = new Computadora();
-
+        //Pedido
         Orden o1 = new Orden();
+
+        //Metemos los ordenadores en el pedido
+        o1.agregarComputadora(pc1);
+        o1.mostrarOrden();
+
     }
 }
